342148e5f979a883cd9da1de2d2ca2f600510dc0
Robert Morton <37875201+abitascout@users.noreply.github.com>
2022-12-15 12:44:42 -0600

    added users

--- sql/users.sql
+++ sql/users.sql
@@ -17,6 +17,20 @@ VALUES(
     "rhm012@email.latech.edu"
 );
 
+INSERT INTO users
+VALUES(
+    "Middle Person",
+    "d93006ec2e4339d770a7afd068c1f1e789a52df12f595e529fd0f302fc1e5ec7",
+    "jmb156@email.latech.edu"
+);
+
+INSERT INTO users
+VALUES(
+    "tugboat2k",
+    "b3c95b46782d66130602473ca592123f99e79d756aed36357f53e0d4239696a5",
+    "trt021@email.latech.edu"
+);
+
 
 
 
