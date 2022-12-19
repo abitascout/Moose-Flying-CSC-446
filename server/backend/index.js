52be7799452fb2cda3bdde96dc743827ff8c7cca
JalenB523 <jmb156@latech.edu>
2022-12-19 12:22:43 -0600

    Login route
    Changed location of index route to be login.
    Added logic to query database and compare.
    --Error, cannot get the auth route that validates users

--- server/backend/index.js
+++ server/backend/index.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const mysql = require("mysql2");
+const path = require("path")
 
 
 const app = express();
+
 app.use(express.json());
 app.use("/", express.static("frontend"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'static')));
+
 
 const PORT = String(process.env.PORT);
 const HOST = String(process.env.HOST);
@@ -14,8 +19,6 @@ const MYSQLHOST = String(process.env.MYSQLHOST);
 const MYSQLUSER = String(process.env.MYSQLUSER);
 const MYSQLPASS = String(process.env.MYSQLPASS);
 const SQL = "SELECT * FROM users;"
-const login = "SELECT username, password FROM users;"
-
 
 let connection = mysql.createConnection({
   host: MYSQLHOST,
@@ -26,7 +29,7 @@ let connection = mysql.createConnection({
 
 
 
-app.get("/query", function (request, response) {
+app.get("/query", (request, response) =>  {
   connection.query(SQL, [true], (error, results, fields) => {
     if (error) {
       console.error(error.message)
@@ -38,16 +41,32 @@ app.get("/query", function (request, response) {
   });
 })
 
-app.get("/login", function (request, response) {
-  connection.query(login, [true], (error, results, fields)=> {
-    if (error) {
-      console.error(error.message)
-      response.status(500).send("database error")
-    } else {
-      console.log(results)
-      response.send(results)
-    }
-  });
+app.get('/', (request, response) => {
+	// Render login template
+	response.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.post("/auth", (req, res) => {
+  // Capture the input fields from the index.html
+  // Reference the name of the input to capture(username, password)
+  let username = req.body.username
+  let password = req.body.password
+  if (username && password) {
+    connection.query("SELECT username, password FROM accounts WHERE username = ? AND password = ?'", [username, sha256(password)], (error, results, fields)=> {
+      if (error) {
+        console.error(error.message)
+        response.status(500).send("database error")
+      }
+      // If we get anything from the database
+      // results object will be populated
+      if ( results.length > 0) {
+        // Redirect to query page
+        res.redirect("/query");
+      } else {
+        res.send("Invalid credentials")
+      }
+    });
+  }
 })
 
 
--- server/frontend/login.js
+++ server/frontend/login.js
@@ -1,5 +1,5 @@
 async function login() {
-    await fetch("http://" + parsedUrl + "/login", {
+    await fetch("http://" + parsedUrl + "/index", {
         method: "GET",
         mode: "no-cors", 
         //redirect: "http://" + parsedUrl.host + "/query"
@@ -7,7 +7,7 @@ async function login() {
     .then((resp) => resp.text())
     .then((data) => {
         
-        document.getElementById("response").innerHTML = data[1];
+        document.getElementById("response").innerHTML = data;
     })
     .catch((err) => {
         console.log(err);
