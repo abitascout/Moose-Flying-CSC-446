81cd4d6eebfecdda1d1a9a82718b3cbea2ba2c15
JalenB523 <75499194+JalenBrown523@users.noreply.github.com>
2023-01-11 17:20:27 -0600

    Update users.sql

--- sql/users.sql
+++ sql/users.sql
@@ -7,34 +7,31 @@ CREATE TABLE users (
     username VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL,
     email    VARCHAR(255) NOT NULL,
+    role     VARCHAR(255) NOT NULL,
     PRIMARY KEY (username)
 );
 
 INSERT INTO users
 VALUES(
     "Robert",
-<<<<<<< Updated upstream
-    "327c670676944cb77685d7a2cc84b51852cd02c21f6e46cfe74a0091b6a4af7d",
-=======
     "$2a$04$ZBcpPXMSGuV0CFmqO4ncDe/WxLLgrVnO9tZWlddBg9Fdk87TKyhwi",
->>>>>>> Stashed changes
-    "rhm012@email.latech.edu"
+    "rhm012@email.latech.edu",
+    "Admin"
 );
 
 INSERT INTO users
 VALUES(
     "Jalen",
-    "d93006ec2e4339d770a7afd068c1f1e789a52df12f595e529fd0f302fc1e",
-    "jmb156@email.latech.edu"
+    "$2a$04$ZBcpPXMSGuV0CFmqO4ncDenf2ouId/JQwsL0wnS7iJ1bRIUGbw9KW",
+    "jmb156@email.latech.edu",
+    "mid"
 );
 
 INSERT INTO users
 VALUES(
     "tugboat2k",
-    "b3c95b46782d66130602473ca592123f99e79d756aed36357f53e0d4239696a5",
-    "trt021@email.latech.edu"
+    "$2a$04$ZBcpPXMSGuV0CFmqO4ncDe8HiGyS9Na3971lcMt.cnt.V5i2bRuoO",
+    "trt021@email.latech.edu",
+    "bottom"
 );
-
-
-
 
