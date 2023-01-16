5bfb65fb14ac8bed3604187cbd61e7035b095116
Robert Morton <Abitascoutrobert1@gmail.com>
2023-01-15 22:47:36 -0600

    testing shit

--- server/backend/index.js
+++ server/backend/index.js
@@ -16,9 +16,15 @@ const HOST = String(process.env.HOST);
 const MYSQLHOST = String(process.env.MYSQLHOST);
 const MYSQLUSER = String(process.env.MYSQLUSER);
 const MYSQLPASS = String(process.env.MYSQLPASS);
+<<<<<<< Updated upstream
 const SQL = "SELECT * FROM users;"
 const login = "SELECT username, password FROM users;"
 
+=======
+const SQL = "SELECT * FROM users;" 
+
+const log = "SELECT *FROM users WHERE username = ? AND password = ?;"
+>>>>>>> Stashed changes
 
 let connection = mysql.createConnection({
   host: MYSQLHOST,
@@ -29,6 +35,7 @@ let connection = mysql.createConnection({
 
 
 
+<<<<<<< Updated upstream
 app.get("/query", function (request, response) {
   
   connection.query(SQL, [true], (error, results, fields) => {
@@ -64,6 +71,105 @@ app.post("/login", function (request, response) {
               response.send("Invaild Username or Password")
           }
         }
+=======
+
+
+app.get('/', (request, response) => {
+	// Render login template
+	response.send("index.html");
+});
+
+
+app.post("/login", (request, response) => {
+  // Capture the input fields from the index.html
+  // Reference the name of the input to capture(username, password) 
+  const username = request.body.username
+  const password = request.body.password
+  const hashPassword = sha256.hashSync(password,salt)
+  const logging = "INSERT INTO logs(username,password,attemp,sessionTime) VALUES ?;"
+  if (username && password) {
+    connection.query(log,[String(username), String(hashPassword)], (error, results)=> {
+      if (error) {
+        console.error(error.message)
+        response.status(500).send("database error")
+        return
+      }
+      // If we get anything from the database
+      // results object will be populated
+      if (results.length > 0) {
+        const user = {name: results[0].username}
+        const ACCESS_TOKEN = require('crypto').randomBytes(64).toString('hex');
+        const roles = {role: results[0].role}
+        // Redirect to query page
+        const token = jwt.sign(roles, ACCESS_TOKEN, {expiresIn: '10s'});
+        //save(ACCESS_TOKEN)
+        const obj = {
+          v1: token,
+          v2: ACCESS_TOKEN,
+          v3: user
+        }
+        const searchParams = new URLSearchParams(obj);
+        const queryString = searchParams.toString();
+        let values = [String(username), String(hashPassword),String("Success"),String("10s")];
+        connection.query(logging, [values], (err,rows) => {
+          if(err) console.log(err);
+          console.log("Logged.")
+        });
+        response.status(200).redirect("query.html?" + queryString);
+        return
+      } else {
+        let values = [String(username), String(hashPassword),"Failed","10s"]
+        connection.query(logging, [values], (err,rows) => {
+          if(err)throw err;
+          console.log("Logged.")
+        });
+        response.status(401).redirect("/")
+        return
+      }
+    });
+  } else {
+    let values = [String(username), String(hashPassword),"Failed","10s"]
+    connection.query(logging, [values], (err,rows) => {
+      if(err)throw err;
+      console.log("Logged.")
+    });
+    response.send("Invalid entry.")
+  }
+});
+
+app.post("/query", (request, response) => 
+{
+  const income = request.body.token;
+  const acc = request.body.acc;
+  const user = request.body.user
+  const itStatement = "SELECT * FROM logs;"
+  const usrStatement = "SELECT * FROM users WHERE username = ?"
+  
+  try{
+    var casing;
+    var varib = true
+    var payload = jwt.verify(income, acc)
+    console.log(payload.role)
+    switch(payload){
+      case "IT":
+        casing = itStatement;
+        break;
+      case "Manager":
+        casing = SQL;
+        break;
+      default:
+        casing = usrStatement;
+        varib = user;
+        break;
+    }    
+    connection.query(casing, [varib], (error, results, fields) => {
+      if (error) {
+        console.error(error.message)
+        response.status(500).send("database error")
+      } else {
+        console.log(results)
+        response.send(results)
+>>>>>>> Stashed changes
       }
     }
   });
--- server/frontend/common.js
+++ server/frontend/common.js
@@ -1,5 +1,6 @@
 var parsedUrl = new URL(window.location.href);
 
+<<<<<<< Updated upstream
 function query() {
     fetch("http://" + parsedUrl.host + "/query", {
         method: "GET",
@@ -12,6 +13,40 @@ function query() {
     .catch((err) => {
         console.log(err);
     })
+=======
+const query = async() =>{
+    const searchParams = new URL (parsedUrl).searchParams;
+    const entries = new URLSearchParams(searchParams).entries();
+    const array = Array.from(entries);
+
+    let obj = {
+        token: array[0][1],
+        acc: array[1][1],
+        user: array[2][2]
+    }
+    const response = await fetch ("http://" + parsedUrl.host + "/query",{
+        method: "POST",
+        body: JSON.stringify(obj),
+        headers: { 'Content-Type': 'application/json; charset=UTF-8' },
+    });
+
+    if(response.status == 401)
+    {
+        alert("Token expired please click exit and log in again")
+        return
+    }
+    const data = response.text();
+    
+    
+    return data
+}
+const beforequery = async () =>{
+    document.getElementById("response").innerHTML = ""
+    const data = await query();
+    if(data != "undefined"){
+        console.log(data)
+        document.getElementById("response").innerHTML = data}
+>>>>>>> Stashed changes
 }
 
 
